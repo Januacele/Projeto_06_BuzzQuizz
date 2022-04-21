f11de75658fa5498e591a883a56a60d3d3382e6c
cgiacometti <camilla.giacometti@gmail.com>
2022-04-21 18:20:41 -0300

    exibição do quizz - respostas aleatorias

--- ScriptTela2.js
+++ ScriptTela2.js
@@ -1,21 +1,41 @@
+//get dos quizzes na API
 let promise = axios.get('https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes');
 promise.then(carregarQuizz);
 
+//carregar página do quizz
 function carregarQuizz(resposta){
     let quizz = resposta.data;
     console.log(quizz)
-    console.log(quizz[9].questions[0].answers.length)
     let cabecalho = document.querySelector(".titulo");
     let pergunta = document.querySelector("main");
+    let num = 18; //id do quizz
 
-    cabecalho.innerHTML = `<img src="${quizz[9].image}" />
-                           <span>${quizz[9].title}</span>`
+    //titulo e imagem do quizz
+    cabecalho.innerHTML = `<img src="${quizz[num].image}" />
+                           <span>${quizz[num].title}</span>`
 
-    for(let i = 0; i < quizz[9].questions.length; i++){
+    //boxes de perguntas
+    for(let i = 0; i < quizz[num].questions.length; i++){
         pergunta.innerHTML += `<div class="pergunta">
-                                    <div class="tituloPergunta">${quizz[9].questions[i].title}</div>
-                                    <img src="${quizz[9].image}" />
-                                 </div>`
+                                    <div class="tituloPergunta">${quizz[num].questions[i].title}</div>
+                               </div>`
     }
 
+    let respostas = document.querySelectorAll(".pergunta");
+
+    //listar respostas em ordem aleatória
+    for(let i = 0; i < teste.length; i++){
+        quizz[num].questions[i].answers.sort(aleatorio); 
+        for(let j = 0; j < quizz[num].questions[i].answers.length; j++){
+            respostas[i].innerHTML += `<div class="resposta">
+                                        <img src="${quizz[num].questions[i].answers[j].image}" />
+                                        <span>${quizz[num].questions[i].answers[j].text}</span>` 
+        }
+    }
+
+}
+
+//embaralha respostas
+function aleatorio(){
+    return Math.random() - 0.5;
 }
