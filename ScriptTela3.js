0825a901ca75620ae58f7d4a73745ba322b01266
Januacele <januacelevieira@hotmail.com>
2022-04-20 23:39:13 -0300

    Layout-Tela3-html-css

---
+++ ScriptTela3.js
@@ -0,0 +1,10 @@
+const API = "https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes";
+let idQuizz = 0;
+let i = 0;
+let QtdPerguntasQuizz = 0;
+
+
+function CriarQuizzComeco (){
+    alert("koeee");
+}
+   
