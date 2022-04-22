0d6515081f452ba864a3dbba80a4dfc07577db8b
Januacele <januacelevieira@hotmail.com>
2022-04-21 22:57:02 -0300

    Layout_Tela3Niveis_Tela4Finalizar

--- Tela3/ScriptTela3.js
+++ Tela3/ScriptTela3.js
@@ -8,17 +8,20 @@ let QtdPerguntasQuizz = 0;
 function trocarTelas3 (){
     const Tela31 = document.querySelector(".Tela31");
     const Tela32 = document.querySelector(".Tela32");
+    const Tela33 = document.querySelector(".Tela33");
+    const Tela34 = document.querySelector(".Tela34");
     const buttom1 = document.querySelector(".Comeco");
     const buttom2 = document.querySelector(".Prosseguir-Niveis");
+    const buttom3 = document.querySelector(".Finalizar-Quizz");
+    const buttom4 = document.querySelector(".voltarHome");
     
-    if (buttom1 === null){
-        Tela31.classList.remove("escondido");
-        Tela32.classList.add("escondido");
-    }
     if (buttom1 !== null){
         Tela31.classList.add("escondido");
         Tela32.classList.remove("escondido"); 
-    }
+    } else if (buttom2 !== null){
+        Tela32.classList.add("escondido");
+        Tela33.classList.remove("escondido"); 
+    }  
 }
 
     
