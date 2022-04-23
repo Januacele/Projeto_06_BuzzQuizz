700b470dc8a24df7bf6715d20a717fe9a8e5ba83
cgiacometti <camilla.giacometti@gmail.com>
2022-04-23 09:57:34 -0300

    TELA 1: layout finalizado

--- ScriptTela1.js
+++ ScriptTela1.js
@@ -5,9 +5,14 @@ promise.then(listarQuizzes);
 
 function listarQuizzes(resposta){
     let quizzes = resposta.data;
+    console.log(quizzes);
     let organizarHTML = document.querySelector(".quizzes");
 
     for(let i = 0; i < quizzes.length; i++){
-        organizarHTML.innerHTML += `<div><img src="${quizzes[i].image}"/></div>`
+        organizarHTML.innerHTML += `<div class="imagem" style="background-image: 
+                                    linear-gradient(180deg, transparent 0%, rgba(255, 255, 255, 0) 0%, 
+                                    rgba(0, 0, 0, 0.5) 65%, #000000 100%), url(${quizzes[i].image})">
+                                    <span>${quizzes[i].title}</span>
+                                    </div>`
     }
 }
